590f6d3cd421e9f4bff7e8350425e7eb88d5c9b6
Jirka Manak <jirka.manak@email.cz>
2020-06-15 17:28:53 +0200

    initial

---
+++ 14_csharp_tridy/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal.Dog dog = new Animal.Dog();
+            dog.NumberOfLegs();
+            dog.MakeSound();
+            Animal.Bird bird = new Animal.Bird();
+            bird.NumberOfLegs();
+            bird.MakeSound();
+            // Console.ReadLine();
+        }
+
+        public class Animal
+        {
+            private int numberOfLegs;
+            private string sound;
+
+            public void NumberOfLegs()
+            {
+                Console.WriteLine("I have {0} legs.", numberOfLegs);
+            }
+
+            public void MakeSound()
+            {
+                Console.WriteLine(sound);
+            }
+
+            public Animal()
+            {
+                Console.WriteLine("{0} has been created.", this.GetType().Name);
+            }
+            public class Dog : Animal
+            {
+                public Dog()
+                {
+                    numberOfLegs = 4;
+                    sound = "Woof! Woof!";
+                }
+            }
+
+            public class Bird : Animal
+            {
+                public Bird()
+                {
+                    numberOfLegs = 2;
+                    sound = "Peep, peep.";
+                }
+            }
+        }
+    }
+}
---
+++ 6_tridici_alg.py
@@ -0,0 +1,8 @@
+""" 
+Slouží k seřazení prvků vstupního seznamu dle požadovaného klíče, na takové seřazení jsou kladeny určité požadavky, z nich nejčastější je rychlost řazení 
+
+Nejoptimálnější nejsou algoritmy, které porovnávají prvky, ale nejsou vhodné k obecnému využití 
+Nejoptimálnější porovnávací algoritmy mají složitost n x log(n), u menších souborů n x n 
+
+Vlastnosti: stabilita, přirozenost
+"""
