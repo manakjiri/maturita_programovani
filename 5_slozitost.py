a28fb8925b3e605de8012e1fd38fa45deaf93f50
Code Simplification <atkarpen@gmail.com>
2020-06-15 22:27:27 +0200

    edit

--- 5_slozitost.py
+++
@@ -1,7 +0,0 @@
-""" 
-Definice: složitost algoritmů udává, jak je daný algoritmus rychlý, pro zjednodušení se zavádí pojem asymptotická složitost, což je rozdělení algoritmů do tříd složitosti, co to znamená, popis dvou algoritmů ze stejné třídy a dvou algoritmů z různých tříd, obrázek 
-
-Složitost se určí jako počet elementárních operací, pro zjednodušení počet porovnání 
-
-Dolní odhad omicron, horní odhad omega, asymptotická shodnost theta
-"""
