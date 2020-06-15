83cb97dc6eb8094ab43d6bdc730d1dcf301724e4
Code Simplification <atkarpen@gmail.com>
2020-06-16 00:18:35 +0200

    sort by topic

---
+++ 15_c#_jednoduche_datove_typy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _15_csharp_datove_typy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
