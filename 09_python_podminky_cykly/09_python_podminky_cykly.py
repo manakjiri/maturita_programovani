8c562a8cc1c5f88d6ffc1f6388bdaa860d5a46b3
Jirka Manak <jirka.manak@email.cz>
2020-06-16 12:37:26 +0200

    ted uz 9 opravdu

---
+++ 09_python_podminky_cykly/09_python_podminky_cykly.py
@@ -0,0 +1,17 @@
+""" 
+if, elif, else
+Cyklus while, cyklus for
+Iterace přes range, přes položky seznamu
+Změna hodnoty řídící proměnné
+- ve for loopu není možná, řídicí proměnná je vždy nastavena na další položku
+"""
+
+# jedine takto
+for i in range(10):
+    if i in (1, 3, 6):
+        continue
+    print(i)
+# přeskočením například, nebo použitím while loopu
+# for loop je dělaný na procházení seznamů
+
+
