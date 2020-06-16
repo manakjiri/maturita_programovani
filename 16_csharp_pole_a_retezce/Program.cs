10546a1397d06177602b6fcd6c49aaad4773ab2d
Code Simplification <atkarpen@gmail.com>
2020-06-16 11:54:12 +0200

    console projects created

---
+++ 16_csharp_pole_a_retezce/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _16_csharp_pole_a_retezce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 17_csharp_osetreni_vyjimek/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _17_csharp_osetreni_vyjimek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
