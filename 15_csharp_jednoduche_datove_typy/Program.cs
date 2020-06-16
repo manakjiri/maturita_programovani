7ddfacdfbef104867e282a9de635e1242ec809e7
Code Simplification <atkarpen@gmail.com>
2020-06-16 23:50:09 +0200

    data type update

--- 15_csharp_jednoduche_datove_typy/Program.cs
+++ 15_csharp_jednoduche_datove_typy/Program.cs
@@ -6,7 +6,10 @@ namespace _15_csharp_datove_typy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sčítání celých čísel");
+            Console.WriteLine(5 + 6 + 11);
+            Console.WriteLine("Sčítání řetězců");
+            Console.WriteLine("5" + "6" + "11");
         }
     }
 }
