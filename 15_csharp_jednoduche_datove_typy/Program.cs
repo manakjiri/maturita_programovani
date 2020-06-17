6bd4fa3c29e4962fb15cb2827c503c945a0c3be5
Code Simplification <atkarpen@gmail.com>
2020-06-17 11:57:57 +0200

    update

--- 15_csharp_jednoduche_datove_typy/Program.cs
+++ 15_csharp_jednoduche_datove_typy/Program.cs
@@ -6,10 +6,37 @@ namespace _15_csharp_datove_typy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Sčítání celých čísel");
-            Console.WriteLine(5 + 6 + 11);
-            Console.WriteLine("Sčítání řetězců");
-            Console.WriteLine("5" + "6" + "11");
+            // Ukázka rozdílu mezi řetězcem (string) a celým číslem (int) 
+
+            string a, b, c;
+            Console.Write("Zadejte první celočíselnou hodnotu: ");
+            a = Console.ReadLine();
+
+            Console.Write("Zadejte druhou celočíselnou hodnotu: ");
+            b = Console.ReadLine();
+
+            Console.Write("Zadejte třetí celočíselnou hodnotu: ");
+            c = Console.ReadLine();
+
+            Console.WriteLine($"Vstupní hodnoty {a}, {b}, {c}");
+
+            Console.WriteLine("Výstup sčítání celých čísel");
+            Console.WriteLine(int.Parse(a) + int.Parse(b) + int.Parse(c));
+
+            Console.WriteLine("Výstup Sčítání řetězců");
+            Console.WriteLine(a + b + c);
+
+            Console.WriteLine();
+
+            // Porovnání rozsahu číselných hodnot různých datových typů
+            Console.WriteLine("Datový typ od Minimální hodnota do Maximální hodnota");
+            Console.WriteLine($"Integer od {int.MinValue} do {int.MaxValue}");
+            Console.WriteLine($"Float od {float.MinValue} do {float.MaxValue}");
+            Console.WriteLine($"Double od {double.MinValue} do {double.MaxValue}");
+            Console.WriteLine($"Decimal od {decimal.MinValue} do {decimal.MaxValue}");
+            
+            // Konec programu
+            Console.ReadLine();
         }
     }
 }
--- 16_csharp_pole_a_retezce/Program.cs
+++ 16_csharp_pole_a_retezce/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+// Knihovna s pomocnými funkcemi (Součet a počet prvků)
+using System.Linq;
+// Knihovna pro práci se seznamem (List)
+using System.Collections.Generic;
 
 namespace _16_csharp_pole_a_retezce
 {
@@ -6,7 +10,26 @@ namespace _16_csharp_pole_a_retezce
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Deklarace a inicializace pole
+            int[] intArr =  new int[] {5, 6, 9, 10};
+
+            // Práce s polem
+            Console.WriteLine($"Vstup: {string.Join(", ", intArr), 10}");
+            Console.WriteLine($"Součet prvků: {intArr.Sum()}");
+            Console.WriteLine($"Počet prvků: {intArr.Count()}");
+
+            // Deklarace a inicializace seznamu
+            List<int> intList = new List<int>();
+            List<object> objectList = new List<object>();
+
+            object[] objArr = new object[] {intArr, 5, "asd", 59};
+            Console.WriteLine($"Vstup: {string.Join(", ", objArr), 10}");
+            Console.WriteLine(((string)objArr[2])[2]);
+            Console.WriteLine($"{objArr[0].GetType().ToString()}, {objArr[1].GetType().ToString()}, {objArr[2].GetType().ToString()}");
+            // Zápis dat do seznamu
+            intList.AddRange(intArr);
+
+            Console.ReadLine();
         }
     }
 }
