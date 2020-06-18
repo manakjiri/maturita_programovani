af7ac8a55f404c7fb228b077f8133d2cd68f94ff
Code Simplification <atkarpen@gmail.com>
2020-06-19 00:32:14 +0200

    sort, dialog & graphics update

--- 06_tridici_algoritmy/6_tridici_alg.py
+++ 06_tridici_algoritmy/6_tridici_alg.py
@@ -6,6 +6,27 @@ Nejoptimálnější porovnávací algoritmy mají složitost n x log(n), u menš
 
 Vlastnosti: stabilita, přirozenost
 """
+from random import randint
 
+def BubbleSort(numList):
+    sorted = False
+    lastIndex = len(numList) - 1
+    while not(sorted):
+        sorted = True
+        for i in range(lastIndex):
+            if numList[i] > numList[i + 1]:
+                numList[i], numList[i + 1] = numList[i + 1], numList[i]
+                sorted = False
+        # print(numList)
+        lastIndex -= 1
 
-
+if __name__ == "__main__":
+    numbers = []
+    for i in range(10):
+        numbers += [randint(0, 100)]
+    print("Zadani:")
+    print(numbers)
+    print()
+    BubbleSort(numbers)
+    print("Vysledek:")
+    print(numbers)
